18d13102954261ec91188cd3e902f8c315461a79
ahoque29 <a.hoque29@hotmail.com>
2024-11-19 00:02:20 +0000

    Create test console project

---
+++ TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess;
+
+namespace TestConsole;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        var databaseBackup = new DatabaseBackup();
+        databaseBackup.BackupDatabase();
+        await using var context = new DatabaseContext();
+        await context.InitializeDatabase();
+    }
+}
